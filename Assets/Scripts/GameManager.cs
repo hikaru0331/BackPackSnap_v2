597352284a7cb2e2f9eb921b8f8131411111e9c9
Koga_hikaru <90139340+hikaru0331@users.noreply.github.com>
2023-05-30 03:14:04 +0900

    [fix]自転車が切れないバグの修正

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,7 +19,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0)) 
+        if(Input.GetMouseButtonDown(0)) 
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
