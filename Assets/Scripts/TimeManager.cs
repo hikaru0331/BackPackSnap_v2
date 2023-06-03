108d2a2a5196f166139338ded8a0952bb8442ae1
Koga_hikaru <90139340+hikaru0331@users.noreply.github.com>
2023-06-03 19:02:28 +0900

    [add]シーンレスゲームオーバー機能の実装

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -3,12 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.EventSystems.EventTrigger;
 
 public class TimeManager : MonoBehaviour
 {
     public Text timerText;
     private float timeCounter = 60.0f;
-    
+
+    public GameObject playingCanvas;
+    public GameObject gameOverCanvas;
+
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +30,25 @@ public class TimeManager : MonoBehaviour
         if (timeCounter <= 0.0f)
         {
             timeCounter = 0.0f;
+
+            //タグの付いたオブジェクトをそれぞれ配列に代入
+            GameObject[] pedestrians = GameObject.FindGameObjectsWithTag("Pedestrian");
+            GameObject[] cycles = GameObject.FindGameObjectsWithTag("Cycle");
+
+            //配列enemyに入ったオブジェクトをすべて破壊するまで繰り返す処理
+            foreach (GameObject enemies in pedestrians)
+            {
+                Destroy(enemies);
+            }
+            foreach(GameObject enemies in cycles)
+            {
+                Destroy(enemies);
+            }
+
+            //UIの切り替え処理
+            playingCanvas.SetActive(false);
+            gameOverCanvas.SetActive(true);
+
         }
     }
 }
